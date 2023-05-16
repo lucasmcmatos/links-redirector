847fe9f336334785b596fe2bc08c4cc3daf0aacc
lucasmcmatos <lucasmmatos05@gmail.com>
2023-05-16 18:16:56 -0300

    Huge Interface Done

--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -6,15 +6,14 @@ var methodOverride = require('method-override')
 router.use(methodOverride('_method'))
 
 router.get("/" , linkController.allLinks)
-
 router.get("/add" , (req , res) =>res.render("../templates/index.ejs" , {err: false, body:{}}))
-
+router.get("/edit/:id" , linkController.loadLink)
 router.get("/:title", linkController.redirect )
 
 router.post("/" , express.urlencoded({extended: true}) , linkController.addLink)
+router.post("/edit/:id" , express.urlencoded({extended: true}) , linkController.editLink)
 
 router.delete("/:id" , linkController.deleteLink);
-
 router.delete("/" , express.json() ,linkController.deleteLink);
 
 module.exports = router;
